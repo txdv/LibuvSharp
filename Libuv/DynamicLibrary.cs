552ce0adb118ed429d90750dc8c95b00b4d83264
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-04-07 06:52:25 +0200

    Expose dynamic library functionality.
    open, close, read symbols.

---
+++ Libuv/DynamicLibrary.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Libuv
+{
+	public class DynamicLibrary
+	{
+		[DllImport("uv")]
+		internal extern static uv_err_t uv_dlopen(IntPtr handle, out IntPtr ptr);
+
+		[DllImport("uv")]
+		internal extern static uv_err_t uv_dlclose(IntPtr handle);
+
+		[DllImport("uv")]
+		internal extern static uv_err_t uv_dlsym(IntPtr handle, string name, out IntPtr ptr);
+
+		IntPtr handle;
+
+		public bool Closed {
+			get {
+				return handle == IntPtr.Zero;
+			}
+		}
+
+		public DynamicLibrary()
+		{
+			UV.EnsureSuccess(uv_dlopen(IntPtr.Zero, out handle));
+		}
+
+		public DynamicLibrary(string str)
+		{
+			var ptr = Marshal.StringToHGlobalAnsi(str);
+			var error = uv_dlopen(ptr, out handle);
+			Marshal.FreeHGlobal(ptr);
+			UV.EnsureSuccess(error);
+		}
+
+		void Close()
+		{
+			if (!Closed) {
+				uv_dlclose(handle);
+				handle = IntPtr.Zero;
+			}
+		}
+
+		public bool TryGetSymbol(string name, out IntPtr pointer)
+		{
+			return uv_dlsym(handle, name, out pointer).code == uv_err_code.UV_OK;
+		}
+
+		public IntPtr GetSymbol(string name)
+		{
+			IntPtr ptr;
+			UV.EnsureSuccess(uv_dlsym(handle, name, out ptr));
+			return ptr;
+		}
+	}
+}
+
