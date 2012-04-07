105a53babee37d0c470072bde42fc8c3a138a869
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-04-07 06:10:43 +0200

    Expose guard functionality.

---
+++ Libuv/Guard.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Libuv
+{
+	public class Guard : BaseHandle
+	{
+		[DllImport("uv")]
+		internal extern static void uv_once(IntPtr ptr, Action callback);
+
+		public Guard()
+			: base(5)
+		{
+		}
+
+		public void Once(Action callback)
+		{
+			var cb = new PermaCallback(callback);
+			uv_once(Handle, cb.Callback);
+		}
+	}
+}
+
