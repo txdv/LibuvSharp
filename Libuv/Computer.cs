96ee8b27eeca126dbd31ce6e27428e4b24c17a05
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-04-07 08:30:14 +0200

    Expose CPU information API.

--- Libuv/Computer.cs
+++ Libuv/Computer.cs
@@ -4,6 +4,78 @@ using System.Runtime.InteropServices;
 
 namespace Libuv
 {
+	unsafe internal struct uv_cpu_times_t
+	{
+		public ulong user;
+		public ulong nice;
+		public ulong sys;
+		public ulong idle;
+		public ulong irq;
+	}
+
+	unsafe internal struct uv_cpu_info_t
+	{
+		public IntPtr model;
+		public int speed;
+		public uv_cpu_times_t cpu_times;
+	}
+
+	public class CpuTimes
+	{
+		internal CpuTimes(uv_cpu_times_t times)
+		{
+			User = times.user;
+			Nice = times.nice;
+			System = times.sys;
+			Idle = times.idle;
+			IRQ = times.irq;
+		}
+
+		public ulong User { get; protected set; }
+		public ulong Nice { get; protected set; }
+		public ulong System { get; protected set; }
+		public ulong Idle { get; protected set; }
+		public ulong IRQ { get; protected set; }
+	}
+
+	unsafe public class CpuInformation
+	{
+		internal CpuInformation(uv_cpu_info_t *info)
+		{
+			Name = Marshal.PtrToStringAnsi(info->model);
+			Speed = info->speed;
+			Times = new CpuTimes(info->cpu_times);
+		}
+
+		public string Name { get; protected set; }
+		public int Speed { get; protected set; }
+		public CpuTimes Times { get; protected set; }
+
+		[DllImport("uv")]
+		internal static extern uv_err_t uv_cpu_info(out IntPtr info, out int count);
+
+		[DllImport("uv")]
+		internal static extern void uv_free_cpu_info(IntPtr info, int count);
+
+		internal static CpuInformation[] GetInfo()
+		{
+			IntPtr info;
+			int count;
+			var error = uv_cpu_info(out info, out count);
+
+			CpuInformation[] ret = new CpuInformation[count];
+
+			for (int i = 0; i < count; i++) {
+				uv_cpu_info_t *cpuinfo = (uv_cpu_info_t *)(info + i*sizeof(uv_cpu_info_t));
+				ret[i] = new CpuInformation(cpuinfo);
+			}
+
+			uv_free_cpu_info(info, count);
+			UV.EnsureSuccess(error);
+			return ret;
+		}
+	}
+
 	unsafe internal struct uv_interface_address_t
 	{
 		public IntPtr name;
@@ -118,6 +190,12 @@ namespace Libuv
 		public static NetworkInterface[] NetworkInterfaces {
 			get {
 				return NetworkInterface.GetInterfaces();
+			}
+		}
+
+		public static CpuInformation[] CpuInfo {
+			get {
+				return CpuInformation.GetInfo();
 			}
 		}
 	}
