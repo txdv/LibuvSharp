40e01c6a281ea8fbacc5e8d94c041d76acac2cc6
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-04-08 01:47:37 +0200

    Expose kill process functionality.
    Add some nice signum enum.

--- Libuv/Process.cs
+++ Libuv/Process.cs
@@ -3,6 +3,44 @@ using System.Runtime.InteropServices;
 
 namespace Libuv
 {
+	public enum Signum : int {
+		SIGHUP    =  1, // Hangup (POSIX).
+		SIGINT    =  2, // Interrupt (ANSI).
+		SIGQUIT   =  3, // Quit (POSIX).
+		SIGILL    =  4, // Illegal instruction (ANSI).
+		SIGTRAP   =  5, // Trace trap (POSIX).
+		SIGABRT   =  6, // Abort (ANSI).
+		SIGIOT    =  6, // IOT trap (4.2 BSD).
+		SIGBUS    =  7, // BUS error (4.2 BSD).
+		SIGFPE    =  8, // Floating-point exception (ANSI).
+		SIGKILL   =  9, // Kill, unblockable (POSIX).
+		SIGUSR1   = 10, // User-defined signal 1 (POSIX).
+		SIGSEGV   = 11, // Segmentation violation (ANSI).
+		SIGUSR2   = 12, // User-defined signal 2 (POSIX).
+		SIGPIPE   = 13, // Broken pipe (POSIX).
+		SIGALRM   = 14, // Alarm clock (POSIX).
+		SIGTERM   = 15, // Termination (ANSI).
+		SIGSTKFLT = 16, // Stack fault.
+		SIGCLD    = SIGCHLD, // Same as SIGCHLD (System V).
+		SIGCHLD   = 17, // Child status has changed (POSIX).
+		SIGCONT   = 18, // Continue (POSIX).
+		SIGSTOP   = 19, // Stop, unblockable (POSIX).
+		SIGTSTP   = 20, // Keyboard stop (POSIX).
+		SIGTTIN   = 21, // Background read from tty (POSIX).
+		SIGTTOU   = 22, // Background write to tty (POSIX).
+		SIGURG    = 23, // Urgent condition on socket (4.2 BSD).
+		SIGXCPU   = 24, // CPU limit exceeded (4.2 BSD).
+		SIGXFSZ   = 25, // File size limit exceeded (4.2 BSD).
+		SIGVTALRM = 26, // Virtual alarm clock (4.2 BSD).
+		SIGPROF   = 27, // Profiling alarm clock (4.2 BSD).
+		SIGWINCH  = 28, // Window size change (4.3 BSD, Sun).
+		SIGPOLL   = SIGIO, // Pollable event occurred (System V).
+		SIGIO     = 29, // I/O now possible (4.2 BSD).
+		SIGPWR    = 30, // Power failure restart (System V).
+		SIGSYS    = 31, // Bad system call.
+		SIGUNUSED = 31
+	}
+
 	[StructLayout(LayoutKind.Sequential)]
 	unsafe internal struct uv_process_options_t : IDisposable
 	{
@@ -179,6 +217,20 @@ namespace Libuv
 
 			return process;
 		}
+
+		[DllImport("uv")]
+		internal static extern int uv_process_kill(IntPtr handle, int signum);
+
+		public void Kill(int signum)
+		{
+			UV.EnsureSuccess(uv_process_kill(handle, signum));
+		}
+
+		public void Kill(Signum signum)
+		{
+			Kill((int)signum);
+		}
+
 	}
 }
 
