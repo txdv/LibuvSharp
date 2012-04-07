9a79e510b991bb1c5d7f84eb2cba1ed8303d5f2c
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-04-08 01:36:00 +0200

    Expose Process spanw functionality.
    Spawn processes!

---
+++ Libuv/Process.cs
@@ -0,0 +1,184 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Libuv
+{
+	[StructLayout(LayoutKind.Sequential)]
+	unsafe internal struct uv_process_options_t : IDisposable
+	{
+		// fields
+
+		public Action<IntPtr, int, int> exit_cb;
+		public IntPtr file;
+		public IntPtr args;
+		public IntPtr env;
+		public IntPtr cwd;
+
+		public int windows_verbatim_arguments;
+
+		public IntPtr stdin_stream;
+		public IntPtr stdout_stream;
+		public IntPtr stderr_stream;
+
+
+		// functions
+
+		public uv_process_options_t(ProcessOptions options, Action<int,int> exitCallback)
+		{
+			if (string.IsNullOrEmpty(options.File)) {
+				throw new ArgumentException("file of processoptions can't be null");
+			} else {
+				file = Marshal.StringToHGlobalAnsi(options.File);
+			}
+
+
+			args = alloc(options.Arguments);
+			env = alloc(options.Environment);
+			cwd = Marshal.StringToHGlobalAnsi(options.CurrentWorkingDirectory);
+
+			windows_verbatim_arguments = (options.WindowsVerbatimArguments ? 1 : 0);
+
+			stdin_stream = IntPtr.Zero;
+			stdout_stream = IntPtr.Zero;
+			stderr_stream = IntPtr.Zero;
+
+			exit_cb = new PermaCallback<IntPtr, int, int>((handle, exit_status, term_signal) => {
+				exitCallback(exit_status, term_signal);
+			}).Callback;
+		}
+
+		public void Dispose()
+		{
+			if (file != IntPtr.Zero) {
+				Marshal.FreeHGlobal(file);
+			}
+
+			free(args);
+			free(env);
+
+			if (cwd != IntPtr.Zero) {
+				Marshal.FreeHGlobal(file);
+			}
+		}
+
+		static IntPtr alloc(string[] args)
+		{
+			if (args == null) {
+				return IntPtr.Zero;
+			}
+
+			var arr = Marshal.AllocHGlobal((args.Length + 1) * sizeof(IntPtr));
+			for (int i = 0; i < args.Length; i++) {
+				Marshal.WriteIntPtr(arr, i * sizeof(IntPtr), Marshal.StringToHGlobalAnsi(args[i]));
+			}
+			Marshal.WriteIntPtr(arr, args.Length * sizeof(IntPtr), IntPtr.Zero);
+			return arr;
+		}
+
+		static void free(IntPtr ptr)
+		{
+			if (ptr == IntPtr.Zero) {
+				return;
+			}
+
+			int i = 0;
+			IntPtr p = Marshal.ReadIntPtr(ptr);
+			while (p != IntPtr.Zero) {
+				Marshal.FreeHGlobal(p);
+				i++;
+				p = Marshal.ReadIntPtr(ptr, i * sizeof(IntPtr));
+			}
+			Marshal.FreeHGlobal(p);
+		}
+	}
+
+	public class ProcessOptions
+	{
+		public string File { get; set; }
+		public string[] Arguments { get; set; }
+		public string[] Environment { get; set; }
+		public string CurrentWorkingDirectory { get; set; }
+		public bool WindowsVerbatimArguments { get; set; }
+	}
+
+	public class Process : Handle
+	{
+		[DllImport("uv")]
+		internal static extern uv_err_t uv_get_process_title(IntPtr buffer, IntPtr size);
+
+		[DllImport("uv")]
+		internal static extern uv_err_t uv_set_process_title(string title);
+
+		public static string Title {
+			get {
+				int size = 512;
+				var ptr = Marshal.AllocHGlobal(size);
+				uv_get_process_title(ptr, (IntPtr)size);
+				string ret = Marshal.PtrToStringAnsi(ptr);
+				Marshal.FreeHGlobal(ptr);
+				return ret;
+			}
+			set {
+				uv_set_process_title(value);
+			}
+		}
+
+		[DllImport("uv")]
+		internal static extern int uv_exepath(IntPtr buffer, ref IntPtr size);
+
+		public static string ExecutablePath {
+			get {
+				int size = 512;
+				var buffer = Marshal.AllocHGlobal(size);
+				var sizePtr = new IntPtr(size);
+				uv_exepath(buffer, ref sizePtr);
+
+				string ret = Marshal.PtrToStringAnsi(buffer);
+				Marshal.FreeHGlobal(buffer);
+				return ret;
+			}
+		}
+
+
+		public Pipe Stdin { get; protected set; }
+		public Pipe Stdout { get; protected set; }
+		public Pipe Stderr { get; protected set; }
+
+		[DllImport("uv")]
+		internal static extern int uv_spawn(IntPtr loop, IntPtr handle, uv_process_options_t options);
+
+		internal Process(Loop loop)
+			: base(loop, UvHandleType.Process)
+		{
+		}
+
+		public static Process Spawn(ProcessOptions options, Action<Process, int, int> exitCallback)
+		{
+			return Spawn(Loop.Default, options, exitCallback);
+		}
+
+		public static Process Spawn(Loop loop, ProcessOptions options, Action<Process, int, int> exitCallback)
+		{
+			Process process = new Process(loop);
+
+			process.Stdin = new Pipe(loop, false);
+			process.Stdout = new Pipe(loop, false);
+			process.Stderr = new Pipe(loop, false);
+
+			uv_process_options_t options_t = new uv_process_options_t(options, (exit_status, term_status) => {
+				options_t.Dispose();
+				exitCallback(process, exit_status, term_status);
+			});
+
+			options_t.stdin_stream = process.Stdin.handle;
+			options_t.stdout_stream = process.Stdout.handle;
+			options_t.stderr_stream = process.Stderr.handle;
+
+			int r = uv_spawn(loop.Handle, process.handle, options_t);
+			UV.EnsureSuccess(r);
+
+			return process;
+		}
+	}
+}
+
