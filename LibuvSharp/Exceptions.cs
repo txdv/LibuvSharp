dddf03c940aac0065c4090452dd970030013a04b
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-05-03 16:39:00 +0200

    Tidy up UVException.
    This class will not be needed once we map all the errors!

--- LibuvSharp/Exceptions.cs
+++ LibuvSharp/Exceptions.cs
@@ -8,17 +8,22 @@ namespace LibuvSharp
 		public int Code { get; protected set; }
 		public string Name { get; protected set; }
 
+		public UVException(int code, string name, string description)
+			: base(string.Format("{0}({1}): {2}", name, code, description))
+		{
+			Code = code;
+			Name = name;
+			Description = description;
+		}
+
 		internal UVException(Loop loop)
 			: this(Ensure.uv_last_error(loop.Handle))
 		{
 		}
 
 		internal UVException(uv_err_t error)
-			: base(string.Format("{0}({1}): {2}", error.Name, error.code, error.Description))
+			: this((int)error.code, error.Name, error.Description)
 		{
-			Description = error.Description;
-			Code = (int)error.code;
-			Name = error.Name;
 		}
 	}
 }
