04515861dd920da72479c5012fb0dca352f0c3be
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-10-04 13:14:08 +0300

    Update AssemblyInfo.

--- LibuvSharp/AssemblyInfo.cs
+++ LibuvSharp/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Libuv")]
+[assembly: AssemblyTitle("LibuvSharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("bentkus")]
+[assembly: AssemblyCopyright("Andrius Bentkus")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
