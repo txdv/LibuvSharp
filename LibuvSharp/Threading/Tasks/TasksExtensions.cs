d62652303538b0decac223818ec5597c3917ed90
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-10-08 01:37:58 +0300

    Add task tcp extensions.

--- LibuvSharp/Threading/Tasks/TasksExtensions.cs
+++ LibuvSharp/Threading/Tasks/TasksExtensions.cs
@@ -6,23 +6,6 @@ namespace LibuvSharp.Threading.Tasks
 {
 	public static class TaskExtensions
 	{
-		public static Task ConnectAsync(this Tcp tcp, string ipAddress, int port)
-		{
-			var tcs = new TaskCompletionSource<object>();
-			try {
-				tcp.Connect(ipAddress, port, (e) => {
-					if (e == null) {
-						tcs.SetResult(null);
-					} else {
-						tcs.SetException(e);
-					}
-				});
-			} catch (Exception e) {
-				tcs.SetException(e);
-			}
-			return tcs.Task;
-		}
-
 		public static Task WaitAsync(this LibuvSharp.Timer timer, TimeSpan timeout)
 		{
 			var tcs = new TaskCompletionSource<object>();
---
+++ LibuvSharp/Threading/Tasks/TcpExtensions.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace LibuvSharp.Threading.Tasks
+{
+	public static class TcpExtensions
+	{
+		public static Task ConnectAsync(this Tcp tcp, IPEndPoint ep)
+		{
+			var tcs = new TaskCompletionSource<object>();
+			try {
+				tcp.Connect(ep, (e) => {
+					if (e == null) {
+						tcs.SetResult(null);
+					} else {
+						tcs.SetException(e);
+					}
+				});
+			} catch (Exception e) {
+				tcs.SetException(e);
+			}
+			return tcs.Task;
+		}
+
+		public static Task ConnectAsync(this Tcp tcp, IPAddress ipAddress, int port)
+		{
+			var tcs = new TaskCompletionSource<object>();
+			try {
+				tcp.Connect(ipAddress, port, (e) => {
+					if (e == null) {
+						tcs.SetResult(null);
+					} else {
+						tcs.SetException(e);
+					}
+				});
+			} catch (Exception e) {
+				tcs.SetException(e);
+			}
+			return tcs.Task;
+		}
+
+		public static Task ConnectAsync(this Tcp tcp, string ipAddress, int port)
+		{
+			var tcs = new TaskCompletionSource<object>();
+			try {
+				tcp.Connect(ipAddress, port, (e) => {
+					if (e == null) {
+						tcs.SetResult(null);
+					} else {
+						tcs.SetException(e);
+					}
+				});
+			} catch (Exception e) {
+				tcs.SetException(e);
+			}
+			return tcs.Task;
+		}
+	}
+}
+
