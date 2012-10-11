fcf6eaee5ca1d9a36f3cffaa22a5bd83a145acd1
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-10-11 18:44:39 +0300

    Rework fs_event wrapper.
    We call it now a FileSystemWatcher, because that is what it does,
    and we add enum FileSystemEvent as events and pass the filename to
    Change as well.

---
+++ LibuvSharp/FileSystemWatcher.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace LibuvSharp
+{
+	public enum FileSystemEventFlags
+	{
+		Default = 0,
+		WatchEntry = 1,
+		Stat = 2,
+		Recursive = 3
+	}
+
+	public enum FileSystemEvent
+	{
+		Rename = 1,
+		Change = 2
+	}
+
+	public class FileSystemWatcher : Handle
+	{
+		delegate void uv_fs_event_cb(IntPtr handle, string filename, int events, int status);
+
+		[DllImport("uv")]
+		private static extern int uv_fs_event_init(IntPtr loop, IntPtr handle, string filename, uv_fs_event_cb callback, int flags);
+
+		uv_fs_event_cb uv_fs_event;
+
+		public string Path { get; protected set; }
+
+		public FileSystemWatcher(string path)
+			: this(path, FileSystemEventFlags.Default)
+		{
+		}
+
+		public FileSystemWatcher(string path, FileSystemEventFlags flags)
+			: this(Loop.Default, path, flags)
+		{
+			Path = path;
+		}
+
+		public FileSystemWatcher(Loop loop, string path)
+			: this(loop, path, FileSystemEventFlags.Default)
+		{
+		}
+
+		public FileSystemWatcher(Loop loop, string path, FileSystemEventFlags flags)
+			: base(loop, HandleType.UV_FS_EVENT)
+		{
+			uv_fs_event = fs_event;
+			int r = uv_fs_event_init(loop.NativeHandle, NativeHandle, path, uv_fs_event, (int)flags);
+			Ensure.Success(r, loop);
+		}
+
+		void fs_event(IntPtr handle, string filename, int events, int status)
+		{
+			if (status != 0) {
+				Close();
+			} else {
+				OnChange(filename, (FileSystemEvent)events);
+			}
+		}
+
+		public event Action<string, FileSystemEvent> Change;
+
+		protected void OnChange(string filename, FileSystemEvent @event)
+		{
+			if (Change != null) {
+				Change(filename, @event);
+			}
+		}
+	}
+}
+
