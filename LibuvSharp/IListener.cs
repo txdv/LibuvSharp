c8e9b1169f67f435c58b0097906a155aedb9e983
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-10-11 17:02:43 +0300

    Clean up IListener.
    Listen with backlog is (UV)Listener specific.

--- LibuvSharp/IListener.cs
+++ LibuvSharp/IListener.cs
@@ -4,8 +4,6 @@ namespace LibuvSharp
 {
 	public interface IListener<TStream> where TStream : IUVStream
 	{
-		int DefaultBacklog { get; set; }
-		void Listen(int backlog);
 		void Listen();
 		event Action IncommingStream;
 		TStream AcceptStream();
