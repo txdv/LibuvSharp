39266c7719facf69031d1ad51d39c98f0f7f1441
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-10-17 01:27:20 +0200

    Add pump utility function.

---
+++ LibuvSharp/Utilities.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace LibuvSharp.Utilities
+{
+	public static class UtilitiesExtensions
+	{
+		public static void Pump(this IUVStream readStream, IUVStream writeStream)
+		{
+			Pump(readStream, writeStream, null);
+		}
+
+		public static void Pump(this IUVStream readStream, IUVStream writeStream, Action<Exception, Exception> callback)
+		{
+			bool pending = false;
+			bool done = false;
+
+			Action<Exception, Exception> call = (ex1, ex2) => {
+				if (done) {
+					return;
+				}
+				done = true;
+				if (callback != null) {
+					callback(ex1, ex2);
+				}
+			};
+
+			readStream.Read((bb) => {
+				writeStream.Write(bb.Buffer, bb.Start, bb.Length);
+				if (writeStream.WriteQueueSize > 0) {
+					pending = true;
+					readStream.Pause();
+				}
+			});
+
+			writeStream.Drain += () => {
+				if (pending) {
+					pending = false;
+					readStream.Resume();
+				}
+			};
+
+			readStream.Complete += () => {
+				readStream.Shutdown();
+				writeStream.Shutdown(() => call(null, null));
+			};
+
+			readStream.Error += (ex) => call(ex, null);
+			readStream.Error += (ex) => call(null, ex);
+
+			readStream.Resume();
+		}
+	}
+}
+
