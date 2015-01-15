b2c7d7d1b796e142b14f3ad91a6602aea82ad2f3
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-01-15 11:57:03 +0100

    Move Async based classes into own files.
    One class, one file. I used to be apposed to this strict rule, but
    this looks way better now.

--- LibuvSharp/Async.cs
+++ LibuvSharp/Async.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace LibuvSharp
@@ -43,97 +42,6 @@ namespace LibuvSharp
 			if (Callback != null) {
 				Callback();
 			}
-		}
-	}
-
-	public class AsyncWatcher<T> : IHandle, IDisposable
-	{
-		Async async;
-		Queue<T> queue = new Queue<T>();
-
-		public AsyncWatcher()
-			: this(Loop.Constructor)
-		{
-		}
-
-		public AsyncWatcher(Loop loop)
-		{
-			async = new Async(loop);
-			async.Callback += () => {
-				Queue<T> tmp;
-				lock (queue) {
-					tmp = new Queue<T>();
-					while (queue.Count > 0) {
-						tmp.Enqueue(queue.Dequeue());
-					}
-				}
-				while (tmp.Count > 0) {
-					OnCallback(tmp.Dequeue());
-				}
-			};
-		}
-
-		public void Ref()
-		{
-			async.Ref();
-		}
-
-		public void Unref()
-		{
-			async.Unref();
-		}
-
-		public bool IsClosed {
-			get {
-				return async.IsClosed;
-			}
-		}
-
-		public void Close(Action callback)
-		{
-			async.Close(callback);
-		}
-
-		public void Dispose()
-		{
-			this.Close();
-		}
-
-		public void Send(T item)
-		{
-			lock (queue) {
-				queue.Enqueue(item);
-			}
-			async.Send();
-		}
-
-		public void Send(IEnumerable<T> data) {
-			Ensure.ArgumentNotNull(data, "data");
-
-			lock (queue) {
-				foreach (var dataitem in data) {
-					queue.Enqueue(dataitem);
-				}
-			}
-			async.Send();
-		}
-
-		public void OnCallback(T item)
-		{
-			if (Callback != null) {
-				Callback(item);
-			}
-		}
-
-		public event Action<T> Callback;
-	}
-
-	public class AsyncCallback : AsyncWatcher<Action>
-	{
-		public AsyncCallback(Loop loop)
-			: base(loop)
-		{
-			Callback += (callback) => callback();
 		}
 	}
 }
