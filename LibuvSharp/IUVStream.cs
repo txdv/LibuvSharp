1f36a8b42cc2552ac82b0618d1895dcd04fa25a0
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-01-23 16:52:27 +0100

    Rename TMessage to TData in IUVStream.
    TMessage contains a TEndPoint and TData.
    
    Or at least it should.

--- LibuvSharp/IUVStream.cs
+++ LibuvSharp/IUVStream.cs
@@ -4,21 +4,21 @@ using System.Text;
 
 namespace LibuvSharp
 {
-	public interface IUVStream<TMessage>
+	public interface IUVStream<TData>
 	{
 		Loop Loop { get; }
 		event Action<Exception> Error;
 
 		bool Readable { get; }
 		event Action Complete;
-		event Action<TMessage> Data;
+		event Action<TData> Data;
 		void Resume();
 		void Pause();
 
 		bool Writeable { get; }
 		event Action Drain;
 		long WriteQueueSize { get; }
-		void Write(TMessage data, Action<Exception> callback);
+		void Write(TData data, Action<Exception> callback);
 		void Shutdown(Action<Exception> callback);
 	}
 }
