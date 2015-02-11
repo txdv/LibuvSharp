c42dc53c56bc1161a4feba43d6207074118d8af6
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-02-11 13:56:39 +0100

    Fix signature of uv_pipe_open.
    Apparently it is always an integer for pipes, but intptr
    on windows/int on linux on the uv_tcp_open and uv_udp_open.

--- LibuvSharp/HandleFileDescriptor.cs
+++ LibuvSharp/HandleFileDescriptor.cs
@@ -46,44 +46,41 @@ namespace LibuvSharp
 		[DllImport("uv", EntryPoint = "uv_tcp_open", CallingConvention = CallingConvention.Cdecl)]
 		internal static extern int uv_tcp_open_windows(IntPtr handle, IntPtr sock);
 
-		[DllImport("uv", EntryPoint = "uv_pipe_open", CallingConvention = CallingConvention.Cdecl)]
-		static extern int uv_pipe_open_unix(IntPtr handle, int fd);
-
-		[DllImport("uv", EntryPoint = "uv_pipe_open", CallingConvention = CallingConvention.Cdecl)]
-		static extern int uv_pipe_open_windows(IntPtr handle, IntPtr fd);
-
 		[DllImport("uv", EntryPoint = "uv_udp_open", CallingConvention = CallingConvention.Cdecl)]
 		internal static extern int uv_udp_open_unix(IntPtr handle, int sock);
 
 		[DllImport("uv", EntryPoint = "uv_udp_open", CallingConvention = CallingConvention.Cdecl)]
 		internal static extern int uv_udp_open_windows(IntPtr handle, IntPtr sock);
 
-		public void Open(Func<IntPtr, int, int> unix, Func<IntPtr, IntPtr, int> windows, IntPtr handle, IntPtr fileDescriptor)
+		[DllImport("uv", CallingConvention = CallingConvention.Cdecl)]
+		static extern int uv_pipe_open(IntPtr handle, int fd);
+
+		public int Open(Func<IntPtr, int, int> unix, Func<IntPtr, IntPtr, int> windows, IntPtr handle, IntPtr fileDescriptor)
 		{
-			int r;
 			if (UV.IsUnix) {
-				r = unix(handle, fileDescriptor.ToInt32());
+				return unix(handle, fileDescriptor.ToInt32());
 			} else {
-				r = windows(handle, fileDescriptor);
+				return windows(handle, fileDescriptor);
 			}
-			Ensure.Success(r);
 		}
 
 		public void Open(IntPtr fileDescriptor)
 		{
+			int r;
 			switch (HandleType) {
 			case HandleType.UV_TCP:
-				Open(uv_tcp_open_unix, uv_tcp_open_windows, NativeHandle, fileDescriptor);
-				break;
-			case HandleType.UV_NAMED_PIPE:
-				Open(uv_pipe_open_unix, uv_pipe_open_windows, NativeHandle, fileDescriptor);
+				r = Open(uv_tcp_open_unix, uv_tcp_open_windows, NativeHandle, fileDescriptor);
 				break;
 			case HandleType.UV_UDP:
-				Open(uv_udp_open_unix, uv_udp_open_windows, NativeHandle, fileDescriptor);
+				r = Open(uv_udp_open_unix, uv_udp_open_windows, NativeHandle, fileDescriptor);
+				break;
+			case HandleType.UV_NAMED_PIPE:
+				r = uv_pipe_open(NativeHandle, fileDescriptor.ToInt32());
 				break;
 			default:
 				throw new NotSupportedException();
 			}
+			Ensure.Success(r);
 		}
 	}
 }
