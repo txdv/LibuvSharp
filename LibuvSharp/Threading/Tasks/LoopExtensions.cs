b971f84cd69bda8fa7650780b3b3e89557bbdded
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-03-17 14:14:08 +0100

    Rename Async and FAsync to QueueUSerWorkItemAsync.
    Since that is what it basically is. A wrapped version
    of QueueUserWorkItemAsync.
    
    The names Async and FAsync are not really clear cut in
    what they do. I tried to name them as a counter part
    to Sync, which Synchronizes something from an external
    thread into the event loop (it runs a delegate in the
    event loop).

--- LibuvSharp/Threading/Tasks/LoopExtensions.cs
+++ LibuvSharp/Threading/Tasks/LoopExtensions.cs
@@ -8,56 +8,51 @@ namespace LibuvSharp.Threading.Tasks
 {
 	public static class LoopExtensions
 	{
-		public static Task Async(this Loop loop, Action action)
+		public static Task QueueUserWorkItemAsync(this Loop loop, Action action)
 		{
 			var tcs = new TaskCompletionSource<object>();
 			Exception exception = null;
-			loop.QueueUserWorkItem(() => {
-				try {
-					action();
-				} catch (Exception e) {
-					exception = e;
-				}
-			}, () => {
-				if (exception == null) {
-					tcs.SetResult(null);
-				} else {
-					tcs.SetException(exception);
-				}
-			});
+			try {
+				loop.QueueUserWorkItem(() => {
+					try {
+						action();
+					} catch (Exception e) {
+						exception = e;
+					}
+				}, () => {
+					if (exception == null) {
+						tcs.SetResult(null);
+					} else {
+						tcs.SetException(exception);
+					}
+				});
+			} catch (Exception ex) {
+				tcs.SetException(ex);
+			}
 			return tcs.Task;
 		}
 
-		public static Task<T> FAsync<T>(this Loop loop, Func<T> action)
+		public static Task<T> QueueUserWorkItemAsync<T>(this Loop loop, Func<T> action)
 		{
 			var tcs = new TaskCompletionSource<T>();
 			Exception exception = null;
 			T res = default(T);
-			loop.QueueUserWorkItem(() => {
-				try {
-					res = action();
-				} catch (Exception e) {
-					exception = e;
-				}
-			}, () => {
-				if (exception == null) {
-					tcs.SetResult(res);
-				} else {
-					tcs.SetException(exception);
-				}
-			});
-			return tcs.Task;
-		}
-
-		public static Task QueueUserWorkItemAsync(this Loop loop, Action work)
-		{
-			var tcs = new TaskCompletionSource<object>();
 			try {
-				loop.QueueUserWorkItem(work, () => {
-					tcs.SetResult(null);
+				loop.QueueUserWorkItem(() => {
+					try {
+						res = action();
+					} catch (Exception e) {
+						exception = e;
+					}
+				}, () => {
+					if (exception == null) {
+						tcs.SetResult(res);
+					} else {
+						tcs.SetException(exception);
+					}
 				});
-			} catch (Exception e) {
-				tcs.SetException(e);
+			} catch (Exception ex) {
+				tcs.SetException(ex);
 			}
 			return tcs.Task;
 		}
