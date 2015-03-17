bcdecfedf0f6765badc043208206a2df573b9c5d
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-03-17 15:31:01 +0100

    Make the FibonacciAsync example more ellaborate.
    This way we can look into how many tasks are running.
    
    I found some odd thing, it is probably dependent on the
    fact that I am running the Tasks very strangely with
    LibuvSharp, since I basically just create the Task and set
    the result or exception.
    
    The thing is in LibuvSharp we don't have a TaskSchedulerer,
    every 'task' gets schedulled immediately. Don't know what
    to make of this, I need some input from someone who everything
    about System.Threading.Tasks.

--- Examples/FibonacciAsync.cs
+++ Examples/FibonacciAsync.cs
@@ -9,6 +9,28 @@ using LibuvSharp.Threading.Tasks;
 
 namespace Test
 {
+	class Job
+	{
+		public Task Task { get; private set; }
+		public DateTime StartTime { get; private set; }
+		public TimeSpan TimeRunning {
+			get {
+				return DateTime.Now - StartTime;
+			}
+		}
+
+		public Job(Task task)
+			: this(task, DateTime.Now)
+		{
+		}
+
+		public Job(Task task, DateTime startTime)
+		{
+			Task = task;
+			StartTime = startTime;
+		}
+	}
+
 	class MainClass
 	{
 		/*
@@ -36,21 +58,21 @@ namespace Test
 		{
 			TimeSpan span = TimeSpan.Zero;
 			BigInteger res = 0;
-			await Loop.Default.QueueUserWorkItemAsync(() => {
+			await Task.Run(() => {
 				var now = DateTime.Now;
+				Console.WriteLine("fib({0}) started {1}", n, now);
 				res = Fibonacci(n);
 				span = DateTime.Now - now;
 			});
 			Console.WriteLine("{0}: fib({1}) = {2}", span, n, res);
 		}
 
-		public static async Task TrackCalculateFibonacci(LinkedList<Task> jobs, int n)
+		public static async Task TrackCalculateFibonacci(LinkedList<Job> jobs, int n)
 		{
-			LinkedListNode<Task> node = null;
+			LinkedListNode<Job> node = null;
 			try {
-				var task = CalculateFibonacci(n);
-				node = jobs.AddLast(task);
-				await task;
+				node = jobs.AddLast(new Job(CalculateFibonacci(n)));
+				await node.Value.Task;
 			} finally {
 				if (node != null) {
 					jobs.Remove(node);
@@ -61,18 +83,19 @@ namespace Test
 		public static void Main(string[] args)
 		{
 			Loop.Default.Run(async () => {
-				LinkedList<Task> jobs = new LinkedList<Task>();
+				var jobs = new LinkedList<Job>();
 				var stdin = new TTY(0);
 				string str = null;
 				while ((str = await stdin.ReadStringAsync()) != null) {
 					str = str.TrimEnd(new char[] { '\r', '\n' });
 					if (str == "quit") {
+						Loop.Current.Stop();
 						break;
 					} else if (str.StartsWith("fib ")) {
 						int n;
 						if (!int.TryParse(str.Substring("fib ".Length), out n)) {
 							Console.WriteLine("Supply an integer to the fib command");
-							return;
+							continue;
 						}
 						TrackCalculateFibonacci(jobs, n);
 					} else if (str == "help") {
@@ -82,6 +105,12 @@ namespace Test
 						Console.WriteLine("quit - quits the program");
 					} else if (str == "count") {
 						Console.WriteLine("Total jobs: {0}", jobs.Count);
+						int i = 0;
+						foreach (var job in jobs) {
+							var awaiter = job.Task.GetAwaiter();
+							Console.WriteLine("{0}: {1} {2} {3}", i, job.StartTime, job.TimeRunning, job.Task.Status);
+							i++;
+						}
 					} else {
 						Console.WriteLine("Unknown command");
 					}
