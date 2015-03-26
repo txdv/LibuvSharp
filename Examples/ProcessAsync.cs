69b887547f031d519d44a67d946d0786fc93c22d
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-03-26 15:04:19 +0100

    Add ProcessAsync example.

---
+++ Examples/ProcessAsync.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using LibuvSharp;
+
+namespace Test
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Loop.Default.Run(async () => {
+				string file = Path.Combine(Directory.GetCurrentDirectory(), "Test.exe");
+				var stdout = new Pipe() { Writeable = true };
+				var stream = Process.Spawn(new ProcessOptions() {
+					Arguments = new string[] { file },
+					File = file,
+					Streams = new UVStream[] { null, stdout },
+				});
+				Console.Write(await stdout.ReadStringAsync());
+			});
+		}
+	}
+}
