0e6f90a2e3429ba07eacab136f4769ceeb14bccf
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-03-31 20:30:28 +0300

    Add explanation to ProcessAsync.

--- Examples/ProcessAsync.cs
+++ Examples/ProcessAsync.cs
@@ -9,6 +9,13 @@ namespace Test
 	{
 		public static void Main(string[] args)
 		{
+			/*
+			 * This works on unix only if .exe files are somehow associated
+			 * with /usr/bin/mono. A good way to test it if they are is to
+			 * set them to executables (chmod +x Test.exe) and then just
+			 * try to run from command line: ./Test.exe
+			 * If they execute, this example should work as well
+			 */
 			Loop.Default.Run(async () => {
 				string file = Path.Combine(Directory.GetCurrentDirectory(), "Test.exe");
 				using (var stdout = new Pipe() { Writeable = true })
